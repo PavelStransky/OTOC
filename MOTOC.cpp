a37ea9be1e534cd3e1d4f7cdc048b0aaff607971
Pavel Stránský <github@pavelstransky.cz>
2020-03-02 12:08:21 +0100

    Adopted for j=50 MOTOC all eigenstates

--- MOTOC.cpp
+++ MOTOC.cpp
@@ -9,29 +9,29 @@
 #include <pthread.h>
 
 // Constants of the Dicke model
-const double omega = 0.5;
-const double omega0 = 0.64899375722968400992;
-const double lambda = 0.66;
+const double omega = 1;
+const double omega0 = 1;
+const double lambda = 2;
 const double lambdac = 0.5*sqrt(omega*omega0);
 
 // Diagonalization parameters
-const int j = 100;
+const int j = 50;
 const int nmax = 300;
 const double G2 = 4.0 * lambda / (sqrt(2.0*j)*omega);	// Parameter of the model
 
-const int numT = 600;									// Number of time steps
-const double startT = -1;								// First time
-const double stepT = 0.01;								// Size of the time step
+const int numT = 256;									// Number of time steps
+const double startT = 1E6;								// First time
+const double stepT = 20000;  							// Size of the time step
 
 // Number of eigenvalues
-const int numev = 16710;
+const int numev = 22548;
 
 // Number of used threads
 const int threads = 32;
 
-#define QMN "./Qmn_f_2.31723_j_100_nmax_300_d_60501_dc_16710.dat"
-#define EVALUES "./EN_f_2.31723_j_100_nmax_300_d_60501_dc_16710.dat"
-#define RESULT "./OTOC_f_2.31723_j_100_nmax_300_k_%d_T_5_tLog_01_pq.dat"
+#define QMN "./j50/qks_f_2._j_50_nmax_300_d_30401_dc_22548.dat"
+#define EVALUES "./j50/EN_f_2._j_50_nmax_300_d_30401_dc_22548.dat"
+#define RESULT "./j50motocs/OTOC_f_2._j_50_nmax_300_k_%d_T_1E6_20000_256_pq.dat"
 	 
 void Error(const char *error) {
 	printf("%s", error);
@@ -112,7 +112,7 @@ void *Thread(void *data) {
 		}
 		
 		result[ti] = dr*dr + di*di;
-		printf("Time %lf (%d) in thread %d calculated in %lds. OTOC = %lf\n", t, ti, td->i, time(0) - start, result[ti]);
+		printf("%d: Time %0.0lf (%d) in thread %d calculated in %lds. OTOC = %lf\n", td->n, t, ti, td->i, time(0) - start, result[ti]);
 	}
 
 	printf("Thread %d has finished.\n", td->i);
@@ -122,56 +122,53 @@ void *Thread(void *data) {
 int main(int argc, char* argv[]) {
 	setbuf(stdout, 0);				// Just to write a message before a line break
 
-	int n = -1;						// Index of the energy we want to calculate [Watch out, C++ starts indexing from 0, while Mathematica from 1]
-	if(argc > 1)
-		n = atoi(argv[1]);
-
-	if(n < 0 || n >= numev)
-		Error("Invalid level to calculate.\n");
-
 	double *qmn = new double[numev*numev];
 	double *eval = new double[numev];
 	Import(qmn, eval);
 
 	double *ts = new double[numT];
 	for(int i = 0; i < numT; i++)
-		ts[i] = pow(10, startT + stepT*i);
+		ts[i] = startT + stepT*i;
 
-	time_t start = time(0);
-	printf("Computing MOTOC for energy E (%d) = %lf in %d threads...\n", n, eval[n], threads);
+    for(int n = 0; n < numev; n++) {
+    	time_t start = time(0);
+	    printf("Computing MOTOC for energy E (%d) = %lf in %d threads...\n", n, eval[n], threads);
 
-	double *result = new double[numT];
+    	double *result = new double[numT];
 
-	pthread_t *threadid = new pthread_t[threads];
-	ThreadData *td = new ThreadData[threads];
+    	pthread_t *threadid = new pthread_t[threads];
+	    ThreadData *td = new ThreadData[threads];
 
-	pthread_attr_t attr;
-	pthread_attr_init(&attr);
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    	pthread_attr_t attr;
+	    pthread_attr_init(&attr);
+	    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
-	for (int i = 0; i < threads; i++) {
-		td[i].i = i;
-		td[i].result = result;
-		td[i].qmn = qmn;
-		td[i].eval = eval;
-		td[i].ts = ts;
-		td[i].n = n;
+        for (int i = 0; i < threads; i++) {
+            td[i].i = i;
+            td[i].result = result;
+            td[i].qmn = qmn;
+            td[i].eval = eval;
+            td[i].ts = ts;
+            td[i].n = n;
 
-		pthread_create(threadid + i, NULL, Thread, td + i);
-	}
+            pthread_create(threadid + i, NULL, Thread, td + i);
+        }
 
-	pthread_attr_destroy(&attr);
+    	pthread_attr_destroy(&attr);
 
-	void *status;
-	for (int i = 0; i < threads; i++) {
-		pthread_join(threadid[i], &status);
-	}
-	printf("\nCalculation finished in %ld seconds.\n", time(0) - start);
+	    void *status;
+    	for (int i = 0; i < threads; i++) {
+	    	pthread_join(threadid[i], &status);
+    	}
+	    printf("\nCalculation finished in %ld seconds.\n", time(0) - start);
 
-	char fname[1024];
-	sprintf(fname, RESULT, n);
+    	char fname[1024];
+	    sprintf(fname, RESULT, n);
 
-	Save(fname, ts, result);
+    	Save(fname, ts, result);
+
+        printf("\n");
+    }
 	
 	delete(qmn);
 	delete(eval);
